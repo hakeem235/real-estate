fcf56a49f73518ffa906c33bdc0861d0a6a4a76c
hakeem235 <hakeemkoo@Gmail.com>
2021-11-28 15:22:34 -0500

    fix bug

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Banner = ({
 }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={500} height={300} alt="banner" />
-    <Box>
+    <Box p="5">
       <Text color="gray.500" fontSize="sm" fontWeight="medium">
         {purpose}
       </Text>
@@ -51,7 +51,7 @@ export default function Home({ propertyForSale, propertyForRent }) {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
-      <Flex flexWrap="wrap">
+      <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
         {propertyForRent.map((property) => <Property property={property} key={property.id}/>) }
       </Flex>
       <Banner
@@ -64,7 +64,7 @@ export default function Home({ propertyForSale, propertyForRent }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      <Flex flexWrap="wrap">
+      <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
      {propertyForSale.map((property) => <Property property={property} key={property.id}/>) }
      </Flex>
     </Box>
